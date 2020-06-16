265db48ca3d5ef62a5f3873c15783978b206adb1
Miha Urbanija <10325909+mihaur@users.noreply.github.com>
2020-06-16 08:45:10 +0200

    refactor: moved logic to services folder

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 import { test } from 'tap'
 import app from '../src/app'
 
-test('GET `/ping` route', async t => {
+test('GET `/api/ping` route', async t => {
   const fastify = app()
   await fastify.ready()
 
@@ -9,16 +9,16 @@ test('GET `/ping` route', async t => {
 
   const res = await fastify.inject({
     method: 'GET',
-    url: '/ping'
+    url: '/api/ping'
   })
-  t.strictEqual(res.statusCode, 200)
   t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.strictEqual(res.statusCode, 200)
   t.deepEqual(res.json(), { ping: 'pong' })
   const result = await fastify.mongo.db.collection('access-log').find({}).limit(1).project({ _id: 0, ping: 1 }).sort({ $natural: -1 }).toArray()
   t.deepEqual(result[0], { ping: 'pong' })
 })
 
-test('GET `/ping/bang` route', async t => {
+test('GET `/api/ping?delay=1` route', async t => {
   const fastify = app()
   await fastify.ready()
 
@@ -26,10 +26,59 @@ test('GET `/ping/bang` route', async t => {
 
   const res = await fastify.inject({
     method: 'GET',
-    url: '/ping/bang'
+    url: '/api/ping?delay=1'
   })
-  t.strictEqual(res.statusCode, 200)
   t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.strictEqual(res.statusCode, 200)
+  t.deepEqual(res.json(), { ping: 'pong' })
+  const result = await fastify.mongo.db.collection('access-log').find({}).limit(1).project({ _id: 0, ping: 1 }).sort({ $natural: -1 }).toArray()
+  t.deepEqual(result[0], { ping: 'pong' })
+})
+
+test('GET `/api/ping?delay=A` route', async t => {
+  const fastify = app()
+  await fastify.ready()
+
+  t.tearDown(fastify.close.bind(fastify))
+
+  const res = await fastify.inject({
+    method: 'GET',
+    url: '/api/ping?delay=A'
+  })
+  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.strictEqual(res.statusCode, 400)
+  t.strictEqual(res.statusMessage, 'Bad Request')
+})
+
+test('GET `/api/ping/bang` route', async t => {
+  const fastify = app()
+  await fastify.ready()
+
+  t.tearDown(fastify.close.bind(fastify))
+
+  const res = await fastify.inject({
+    method: 'GET',
+    url: '/api/ping/bang'
+  })
+  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.strictEqual(res.statusCode, 200)
+  t.deepEqual(res.json(), { ping: 'bang' })
+  const result = await fastify.mongo.db.collection('access-log').find({}).limit(1).project({ _id: 0, ping: 1 }).sort({ $natural: -1 }).toArray()
+  t.deepEqual(result[0], { ping: 'bang' })
+})
+
+test('GET `/api/ping/bang?delay=1` route', async t => {
+  const fastify = app()
+  await fastify.ready()
+
+  t.tearDown(fastify.close.bind(fastify))
+
+  const res = await fastify.inject({
+    method: 'GET',
+    url: '/api/ping/bang?delay=1'
+  })
+  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.strictEqual(res.statusCode, 200)
   t.deepEqual(res.json(), { ping: 'bang' })
   const result = await fastify.mongo.db.collection('access-log').find({}).limit(1).project({ _id: 0, ping: 1 }).sort({ $natural: -1 }).toArray()
   t.deepEqual(result[0], { ping: 'bang' })
