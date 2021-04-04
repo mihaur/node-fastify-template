7c88c2dfe765c8124458eb8331b82d89a91abcd8
Miha Urbanija <10325909+mihaur@users.noreply.github.com>
2021-04-04 20:43:03 +0200

    📦 resolve deprecate messages in tests

--- test/app.test.js
+++ test/app.test.js
@@ -5,81 +5,81 @@ test('GET `/api/ping` route', async t => {
   const fastify = await app()
   await fastify.ready()
 
-  t.tearDown(fastify.close.bind(fastify))
+  t.teardown(fastify.close.bind(fastify))
 
   const res = await fastify.inject({
     method: 'GET',
     url: '/api/ping'
   })
-  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.strictEqual(res.statusCode, 200)
-  t.deepEqual(res.json(), { ping: 'pong' })
+  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.equal(res.statusCode, 200)
+  t.same(res.json(), { ping: 'pong' })
   const result = await fastify.mongo.db.collection('access-log').find({}).limit(1).project({ _id: 0, ping: 1 }).sort({ $natural: -1 }).toArray()
-  t.deepEqual(result[0], { ping: 'pong' })
+  t.same(result[0], { ping: 'pong' })
 })
 
 test('GET `/api/ping?delay=1` route', async t => {
   const fastify = await app()
   await fastify.ready()
 
-  t.tearDown(fastify.close.bind(fastify))
+  t.teardown(fastify.close.bind(fastify))
 
   const res = await fastify.inject({
     method: 'GET',
     url: '/api/ping?delay=1'
   })
-  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.strictEqual(res.statusCode, 200)
-  t.deepEqual(res.json(), { ping: 'pong' })
+  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.equal(res.statusCode, 200)
+  t.same(res.json(), { ping: 'pong' })
   const result = await fastify.mongo.db.collection('access-log').find({}).limit(1).project({ _id: 0, ping: 1 }).sort({ $natural: -1 }).toArray()
-  t.deepEqual(result[0], { ping: 'pong' })
+  t.same(result[0], { ping: 'pong' })
 })
 
 test('GET `/api/ping?delay=A` route', async t => {
   const fastify = await app()
   await fastify.ready()
 
-  t.tearDown(fastify.close.bind(fastify))
+  t.teardown(fastify.close.bind(fastify))
 
   const res = await fastify.inject({
     method: 'GET',
     url: '/api/ping?delay=A'
   })
-  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.strictEqual(res.statusCode, 400)
-  t.strictEqual(res.statusMessage, 'Bad Request')
+  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.equal(res.statusCode, 400)
+  t.equal(res.statusMessage, 'Bad Request')
 })
 
 test('GET `/api/ping/bang` route', async t => {
   const fastify = await app()
   await fastify.ready()
 
-  t.tearDown(fastify.close.bind(fastify))
+  t.teardown(fastify.close.bind(fastify))
 
   const res = await fastify.inject({
     method: 'GET',
     url: '/api/ping/bang'
   })
-  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.strictEqual(res.statusCode, 200)
-  t.deepEqual(res.json(), { ping: 'bang' })
+  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.equal(res.statusCode, 200)
+  t.same(res.json(), { ping: 'bang' })
   const result = await fastify.mongo.db.collection('access-log').find({}).limit(1).project({ _id: 0, ping: 1 }).sort({ $natural: -1 }).toArray()
-  t.deepEqual(result[0], { ping: 'bang' })
+  t.same(result[0], { ping: 'bang' })
 })
 
 test('GET `/api/ping/bang?delay=1` route', async t => {
   const fastify = await app()
   await fastify.ready()
 
-  t.tearDown(fastify.close.bind(fastify))
+  t.teardown(fastify.close.bind(fastify))
 
   const res = await fastify.inject({
     method: 'GET',
     url: '/api/ping/bang?delay=1'
   })
-  t.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.strictEqual(res.statusCode, 200)
-  t.deepEqual(res.json(), { ping: 'bang' })
+  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  t.equal(res.statusCode, 200)
+  t.same(res.json(), { ping: 'bang' })
   const result = await fastify.mongo.db.collection('access-log').find({}).limit(1).project({ _id: 0, ping: 1 }).sort({ $natural: -1 }).toArray()
-  t.deepEqual(result[0], { ping: 'bang' })
+  t.same(result[0], { ping: 'bang' })
 })
