6c691635ed20f60fe7a982002ce3d2e6a2469c69
Miha Urbanija <10325909+mihaur@users.noreply.github.com>
2022-11-22 21:43:07 +0100

    refactor: use setTimeout from node:timers/promises instead of Promise

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,4 @@
-export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-// export async function processRequest (response, service, result, collection) {
-//   await logAccess('headers', result, fastify.mongo.db.collection('access-log'))
-//   await sleep(request.query.delay)
-//   // nosemgrep: javascript.express.security.audit.xss.direct-response-write.direct-response-write
-//   response.send(result)
-// }
+export { setTimeout as sleep } from 'node:timers/promises'
 
 export async function logAccess (service, document, collection) {
   const result = { service, ...document, createdAt: new Date() }
