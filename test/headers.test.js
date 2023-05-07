32dbfc4cd5439c6a9a692a83018028163d5779ed
Miha Urbanija <10325909+mihaur@users.noreply.github.com>
2023-05-07 21:23:22 +0200

    test: switch to node build in test runner

--- test/headers.test.js
+++ test/headers.test.js
@@ -1,77 +1,79 @@
-import { test } from 'tap'
+import { test, beforeEach, afterEach } from 'node:test'
+import { strict as assert } from 'node:assert'
 import { buildFastify, getLastLogItem } from './helper.js'
 
 const lastLogItem = getLastLogItem('headers')
 
 /* jscpd:ignore-start */
+beforeEach(async (t) => {
+  t.app = await buildFastify()
+})
+
+afterEach(async (t) => {
+  t.app.close()
+})
+
 test('GET `/api/headers` route', async t => {
-  const app = await buildFastify(t)
   const expectedHeaders = {
     'user-agent': 'lightMyRequest',
     host: 'localhost:80'
   }
 
-  const res = await app.inject({
+  const res = await t.app.inject({
     method: 'GET',
     url: '/api/headers'
   })
 
-  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.equal(res.statusCode, 200)
-  t.same(res.json(), expectedHeaders)
-  t.same(await lastLogItem(app.mongo.db), expectedHeaders)
+  assert.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  assert.equal(res.statusCode, 200)
+  assert.deepEqual(res.json(), expectedHeaders)
+  assert.deepEqual(await lastLogItem(t.app.mongo.db), expectedHeaders)
 })
 
 test('GET `/api/headers` route additional headers', async t => {
-  const app = await buildFastify(t)
-
   const expectedHeaders = {
-    'x-my-header': 42,
+    'x-my-header': '42',
     'user-agent': 'lightMyRequest',
     host: 'localhost:80'
   }
 
-  const res = await app.inject({
+  const res = await t.app.inject({
     method: 'GET',
     url: '/api/headers',
-    headers: { 'x-my-header': 42 }
+    headers: { 'x-my-header': '42' }
   })
 
-  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.equal(res.statusCode, 200)
-  t.same(res.json(), expectedHeaders)
-  t.same(await lastLogItem(app.mongo.db), expectedHeaders)
+  assert.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  assert.equal(res.statusCode, 200)
+  assert.deepEqual(res.json(), expectedHeaders)
+  assert.deepEqual(await lastLogItem(t.app.mongo.db), expectedHeaders)
 })
 
 test('GET `/api/headers?delay=1` route', async t => {
-  const app = await buildFastify(t)
-
   const expectedHeaders = {
     'user-agent': 'lightMyRequest',
     host: 'localhost:80'
   }
 
-  const res = await app.inject({
+  const res = await t.app.inject({
     method: 'GET',
     url: '/api/headers'
   })
 
-  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.equal(res.statusCode, 200)
-  t.same(res.json(), expectedHeaders)
-  t.same(await lastLogItem(app.mongo.db), expectedHeaders)
+  assert.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  assert.equal(res.statusCode, 200)
+  assert.deepEqual(res.json(), expectedHeaders)
+  assert.deepEqual(await lastLogItem(t.app.mongo.db), expectedHeaders)
 })
 
 test('GET `/api/headers?delay=A` route', async t => {
-  const app = await buildFastify(t)
-
-  const res = await app.inject({
+  const res = await t.app.inject({
     method: 'GET',
     url: '/api/headers?delay=A'
   })
 
-  t.equal(res.headers['content-type'], 'application/json; charset=utf-8')
-  t.equal(res.statusCode, 400)
-  t.equal(res.statusMessage, 'Bad Request')
+  assert.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+  assert.equal(res.statusCode, 400)
+  assert.equal(res.statusMessage, 'Bad Request')
 })
 /* jscpd:ignore-end */
